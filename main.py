e507fec97f665489b811aee9df3afcb5c375cc2f
ANIL KUMAR CHINTHAPALLI <111039342+anilkumar-011@users.noreply.github.com>
2024-10-22 14:59:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+# CORS(app, origins=["http://localhost:3000"], methods=["GET", "POST", "DELETE","PUT"], allow_headers=["*"])
+
+# flask_cors.cross_origin(
+#     origins="http://127.0.0.1:9000/",
+#     methods=["GET", "HEAD", "POST", "OPTIONS", "PUT"],
+#     headers=None,
+#     supports_credentials=False,
+#     max_age=None,
+#     send_wildcard=True,
+#     always_send=True,
+#     automatic_options=False,
+# )
+
+
+@app.after_request
+def after_request(response):
+    # Add custom headers to the response
+    response.headers["access-control-allow-methods"] = "DELETE, GET, POST, PUT"
+    response.headers["access-control-allow-origin"] = "http://localhost:3000"
+    response.headers["access-control-allow-headers"] = "Content-Type, Authorization, Anil"
+    return response
+
+@app.route("/")
+def home():
+    return "Hello, World! from server 2"
+
+
+@app.route("/api/data", methods=['GET', 'POST', 'PUT'])
+def api_data():
+    if request.method == 'GET':
+        data = {"message": "Hello, this is your data!", "status": "success"}
+        return jsonify(data)
+
+    elif request.method == 'POST':
+        incoming_data = request.json
+        message = incoming_data.get('message', 'No message received')
+        
+        response_data = {
+            "message": f"Received: {message}",
+            "status": "success"
+        }
+        return jsonify(response_data)
+
+    elif request.method == 'PUT':
+        incoming_data = request.json
+        message = incoming_data.get('message', 'No message received')
+        
+        response_data = {
+            "message": f"Updated: {message}",
+            "status": "success"
+        }
+        return jsonify(response_data)
+
+@app.route('/api/delete', methods=['DELETE', 'OPTIONS'])
+def delete():
+    response_data = {
+        "message": "Data deleted successfully",
+        "status": "success"
+    }
+    return jsonify(response_data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
